2a33eb944656974004cabafb7123f0a0ac3ecbd9
Trịnh Văn Đạt <53535112+trinhvandat@users.noreply.github.com>
2023-05-17 15:55:11 +0700

    Feature/#3 config server close #3 (#5)
    * create config-server
    
    * finish add config server with configuration file in backend system
    
    ---------
    
    Co-authored-by: Dat Trinh Van <dat.t@cimbthai.com>

--- greeting-service/src/main/java/com/leonard/greeting/MainApplication.java
+++ greeting-service/src/main/java/com/leonard/greeting/MainApplication.java
@@ -1,15 +1,20 @@
 package com.leonard.greeting;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @Slf4j
 public class MainApplication implements CommandLineRunner {
+
+  @Autowired
+  private Environment environment;
 
   public static void main(String[] args) {
     SpringApplication.run(MainApplication.class, args);
@@ -18,5 +23,6 @@ public class MainApplication implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     log.info("Greeting service is running...");
+    log.info("test env from config-server: [{}]", environment.getProperty("test.message"));
   }
 }
